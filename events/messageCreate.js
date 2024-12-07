6ab87b04cff7e3269e9e8ed1382bdf8617739c9f
Ace <stolenfvx@gmail.com>
2024-12-07 19:43:35 +0100

    Initial commit

---
+++ events/messageCreate.js
@@ -0,0 +1,82 @@
+const GuildSettings = require('../models/GuildSettings');
+const UserSettings = require('../models/UserSettings');
+require('dotenv').config();
+const { ActionRowBuilder, EmbedBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+    name: 'messageCreate',
+    async execute(message, client) {
+        // Ignore messages from bots
+        if (message.author.bot) return;
+
+        try {
+
+            // Retrieve guild prefix from the database or register with default prefix
+            const [guildSettings, created] = await GuildSettings.findOrCreate({
+                where: { guildId: message.guild.id },
+                defaults: { prefix: process.env.PREFIX },
+            });
+
+            // Extract prefix from guildSettings
+            const serverPrefix = guildSettings.prefix;
+            
+            // Check if the bot is mentioned in the message
+            if (message.mentions.has(client.user)) {
+                const mentionReply = new EmbedBuilder()
+                    .setAuthor({ name: `${message.author.tag}`, iconURL: message.author.displayAvatarURL() })
+                    .setDescription(`Sup ${message.author.tag}!\nMy prefix here: \`${serverPrefix}\`\n\n Use \`${serverPrefix}help\` to get my command's list.`)
+                    .setColor(client.noColorEmbed)
+                    .setFooter({ text: "IDK what to put here!", iconURL: client.user.displayAvatarURL() });
+                
+                const inviteMe = new ButtonBuilder()
+                    .setLabel('Invite Me')
+                    .setURL('https://discord.com/oauth2/authorize?client_id=1248329993193263134&permissions=8&integration_type=0&scope=bot+applications.commands')
+                    .setStyle(ButtonStyle.Link);
+                
+                const support = new ButtonBuilder()
+                    .setLabel('Support')
+                    .setURL('https://discord.gg/W7zen4gGbv')
+                    .setStyle(ButtonStyle.Link);
+                
+                const row = new ActionRowBuilder()
+                    .addComponents(inviteMe, support);
+                await message.reply({ embeds: [mentionReply], components: [row], allowedMentions: { repliedUser: false} });
+            }
+        
+            // Check if the message starts with the prefix
+            if (!message.content.startsWith(serverPrefix)) return;
+
+            // Extract command and arguments from the message
+            const args = message.content.slice(serverPrefix.length).trim().split(/ +/);
+            const commandName = args.shift().toLowerCase();
+
+            // Get the command file corresponding to the command name
+            const command = client.prefixedCommands.get(commandName) 
+                || Array.from(client.prefixedCommands.values()).find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+            // If the command doesn't exist, exit
+            if (!command) return;
+
+            // If the command is an owner command
+            if (command.owner && message.author.id !== process.env.OWNER_ID) return;
+
+            // Check if the user is blocked
+            const userSettings = await UserSettings.findOne({ where: { userId: message.author.id } });
+            if (userSettings && userSettings.blocked) {
+                const embed = new EmbedBuilder()
+                    .setColor('#FF0000')
+                    .setTitle('Blocked User')
+                    .setDescription('You are blocked from using this bot.');
+
+                return message.reply({ embeds: [embed], ephemeral: true });
+            }
+
+            // Execute the command
+            await command.execute(message, args);
+
+        } catch (error) {
+            console.error('Error executing command:', error);
+            message.reply({ content: 'There was an error executing that command.', ephemeral: true });
+        }
+    },
+};
---
+++ events/ready.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const { ActivityType } = require("discord.js");
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    async execute(client) {
+      console.log(`Ready! Logged in as ${client.user.tag}`);
+      
+      const guild = client.guilds.cache.get(process.env.GUILD_ID);
+      if (guild) {
+        await guild.commands.set(client.commands.map(cmd => cmd.data));
+        console.log('Guild Commands registered');
+      } else {
+        console.log('Guild not found');
+      }
+
+      client.user.setPresence({ activities: [{ name: "Made by Ace." ,type: ActivityType.Listening }], status: 'idle' });
+    },
+  };
+  
