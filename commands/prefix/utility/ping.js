edaa64cf89eee63ad2f8cfef34ec6a05d229f697
Ace <stolenfvx@gmail.com>
2024-12-11 21:31:01 +0100

    Updated reply message

--- commands/prefix/utility/ping.js
+++ commands/prefix/utility/ping.js
@@ -3,7 +3,6 @@ const { PermissionsBitField, EmbedBuilder } = require('discord.js');
 module.exports = {
     name: 'ping',
     description: 'Check the bot\'s latency to the Discord API and WebSocket.',
-    usage: '',
     requiredPermissions: [PermissionsBitField.Flags.SendMessages, PermissionsBitField.Flags.EmbedLinks],
     userPermissions: [],
     owner: false,
@@ -13,8 +12,8 @@ module.exports = {
 
         // Create embed
         const embed = new EmbedBuilder()
-        	.setColor('2B2D31')
-          .setDescription(`Pong in \`${websocketHeartbeat}ms\``);
+        	.setColor(message.client.noColor)
+          .setAuthor({ name: `Pong in \`${websocketHeartbeat}ms\``, iconURL: message.client.user.displayAvatarURL() });
 
         // Edit the sent message with the embed
         await message.reply({ embeds: [embed], allowedMentions: { repliedUser: false} });
