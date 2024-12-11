029fd1fe1987fb4a9532ec8a637e04be5b1291df
Ace <stolenfvx@gmail.com>
2024-12-11 21:06:35 +0100

    Updated reply message

--- commands/prefix/fm/stats.js
+++ commands/prefix/fm/stats.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 module.exports = {
     name: 'stats',
     description: 'Displays a summary of the user\'s listening habits.',
-    usage: '',
     requiredPermissions: [PermissionsBitField.Flags.EmbedLinks],
     userPermissions: [],
     owner: false,
@@ -21,7 +20,7 @@ module.exports = {
                 const noUserEmbed = new EmbedBuilder()
                     .setColor('#FF0000')
                     .setDescription(
-                        "You haven't set your Last.fm username yet. Use the `!setlastfm` command to link your account."
+                        "You haven't set your Last.fm account yet. Use the `!setlastfm` command to link your account."
                     );
                 return await message.reply({ embeds: [noUserEmbed], allowedMentions: { repliedUser: false } });
             }
